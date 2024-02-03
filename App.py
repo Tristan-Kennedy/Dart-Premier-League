8b765c4a1270ee43e7a0df9886a377b7938b5053
Tristan Kennedy <tdouglaskennedy@gmail.com>
2024-02-03 16:48:07 -0600

    Add simple player functions

--- App.py
+++ App.py
@@ -1,10 +1,13 @@
 from PySide6.QtWidgets import QApplication
 from Dartboard import Dartboard
-from Scoreboard import Scoreboard
+from Scoreboard import *
 
 # Run this to start the program
 app = QApplication([])
-scoreboard = Scoreboard()
+player1 = Player("Test Player 1")
+player2 = Player("Test Player 2")
+players = [player1, player2]
+scoreboard = Scoreboard(players)
 dartboard = Dartboard(scoreboard)
 dartboard.show()
 scoreboard.show()
--- Scoreboard.py
+++ Scoreboard.py
@@ -1,17 +1,35 @@
 from PySide6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
 
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 501
+
 class Scoreboard(QMainWindow):
-    def __init__(self):
+    def __init__(self, players):
         super().__init__()
         self.setWindowTitle("ScoreBoard")
-        self.score = 0
-        self.scoreLabel = QLabel(str(self.score))
+        self.players = players
+        self.current_player_index = 0
+        self.turns = 0
+
+        self.scoreLabels = [QLabel(self.get_score_text(player)) for player in self.players]
+        
         layout = QVBoxLayout()
-        layout.addWidget(self.scoreLabel)
+        for scoreLabel in self.scoreLabels:
+            layout.addWidget(scoreLabel)
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
 
+    def get_score_text(self, player):
+        return f"{player.name}: {player.score}"
+
     def update_score(self, score):
-        self.score = score
-        self.scoreLabel.setText(str(self.score))
+        self.players[self.current_player_index].score -= score
+        self.scoreLabels[self.current_player_index].setText(self.get_score_text(self.players[self.current_player_index]))
+        self.turns += 1
+
+        if self.turns >= 3:
+            self.turns = 0
+            self.current_player_index = (self.current_player_index + 1) % len(self.players)
