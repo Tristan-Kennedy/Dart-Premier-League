1488ce91a049fb0e2f6e1a2b3b084348cb741e87
Aadvil <146052904+Aadvil@users.noreply.github.com>
2024-03-02 14:50:04 -0600

    Add files via upload

---
+++ src/Foul.py
@@ -0,0 +1,38 @@
+#attempt at making a knockout/foul button
+from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QPainter, QPen, QBrush, QPainterPath, QColor, QFont
+from PySide6.QtCore import Qt, QPoint, QRectF, Signal
+
+class Foul(QWidget):
+    #Basic Widget Construction 
+    foul_click = Signal(int)
+
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 50, 50)
+        self.setWindowTitle('Foul')
+
+        self.setMouseTracking(True)
+
+    #Paint the button
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(Qt.black, 2))
+        painter.setBrush(QBrush(QColor(176, 200, 112)))
+        button_size = 75
+        button_rect = QRectF((self.width() - button_size) / 2, (self.height() - button_size) / 2, button_size, button_size)
+        painter.drawRect(button_rect) #draw the button with our custom size 
+        
+        
+    #add text
+        text = "FOUL"
+        font = QFont("Comic Sans MS", 10) #pick a font. I dare you to find one more cursed than this
+        painter.setFont(font)
+        text_rect = QRectF(0, 0, self.width(), self.height())
+        painter.drawText(text_rect, Qt.AlignCenter, text) 
+    
+
+        
+
+        
--- src/ScorekeeperUI.py
+++ src/ScorekeeperUI.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout
 from .Dartboard import *
 from .Settings import *
+from .Knockout import *
+from .Foul import Foul
 
 class ScorekeeperUI(QMainWindow):
     def __init__(self):
@@ -8,11 +10,15 @@ class ScorekeeperUI(QMainWindow):
 
         # Create a Dartboard instance
         self.dartboard = Dartboard()
+        self.knockout = Knockout()
+        self.foul = Foul()
         self.settings = Settings()
 
         # Create a QVBoxLayout to hold the Dartboard
         layout = QVBoxLayout()
         layout.addWidget(self.dartboard, 5)
+        layout.addWidget(self.knockout, 4)
+        layout.addWidget(self.foul, 4)
         layout.addWidget(self.settings, 1)
 
         # Create a central widget to hold the layout
